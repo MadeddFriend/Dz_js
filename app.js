d762da1a7f5c5cb054307a91a948dbcdf2cdba00
MadeddFriend <madeddfriend@gmail.com>
2025-01-03 13:00:10 +0300

    Решение задачи

---
+++ app.js
@@ -0,0 +1,14 @@
+const arr = [1, 40, -5, 10, 0]
+
+for (let i = 0; i < arr.length - 1; i++) {
+	for (let j = 0; j < arr.length - 1 - i; j++) {
+		if (arr[j] > arr[j + 1]) {
+			// Обмен элементов местами
+			const temp = arr[j]
+			arr[j] = arr[j + 1]
+			arr[j + 1] = temp
+		}
+	}
+}
+
+console.log(arr)
